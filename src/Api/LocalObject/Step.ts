1d22c39d563e9a4f5eeae9ab438b1f8ec62cd1dc
Ririena <nerllysurina@gmail.com>
2024-02-22 00:20:42 +0700

    Local Object

---
+++ src/Api/LocalObject/Step.ts
@@ -0,0 +1,21 @@
+export const purchaseSteps = [
+  {
+    stepNumber: 1,
+    title: "Pilih Produk",
+    description: "Pilih produk kopi yang ingin Anda beli dari daftar di atas.",
+    image: "/service-1.png",
+  },
+  {
+    stepNumber: 2,
+    title: "Deliver",
+    description:
+      "Dapatkan pesanan Anda dengan cepat dan mudah dengan AstralExpress.",
+    image: "/service-2.png",
+  },
+  {
+    stepNumber: 3,
+    title: "Konsumsi Kopi",
+    description: "Nikmati segarnya kopi berkualitas di mana pun Anda berada.",
+    image: "/service-3.png",
+  },
+];
